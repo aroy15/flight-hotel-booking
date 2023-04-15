21b5eb6939cc05f54ac37d15ff3237a95541563d
aroy15 <anjonroy57@gmail.com>
2023-04-16 00:09:52 +0600

    sidebar & dashboard layout updated

--- client/src/Layout/DashboardLayout.js
+++ client/src/Layout/DashboardLayout.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Sidebar from '../Components/Dashboard/Sidebar';
 
 const DashboardLayout = () => {
     return (
-        <div>
-           <div>sidebar</div>
-           <div>
-            <Outlet/>
-           </div>
+        <div className="md:flex relative min-h-screen">
+            <Sidebar />
+            <div className='flex-1'>
+                <div className='p-5'>
+                    <Outlet/>
+                </div>
+            </div>
         </div>
     );
 };
