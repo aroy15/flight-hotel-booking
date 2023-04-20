a419e524f6d8026339287eb9c72e9685252d178f
aroy15 <anjonroy57@gmail.com>
2023-04-20 11:36:57 +0600

    add home api creation

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
-const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config();
 
 const app = express();
@@ -14,48 +14,55 @@ app.use(express.json())
 // Database Connection
 const uri = process.env.DB_URI
 
-const client = new MongoClient(uri, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    serverApi: ServerApiVersion.v1 
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const homesCollection = client.db('hotel-flight-db').collection('homes');
         const usersCollection = client.db('hotel-flight-db').collection('users');
         const bookingsCollection = client.db('hotel-flight-db').collection('bookings');
 
         // Save user email & generate JWT
-        app.put('/user/:email', async (req, res)=>{
+        app.put('/user/:email', async (req, res) => {
             const email = req.params.email
             const user = req.body
-            const filter = {email: email}
-            const options = {upsert: true}
+            const filter = { email: email }
+            const options = { upsert: true }
             const updateDoc = {
-                $set:user,                
+                $set: user,
             }
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             console.log(result)
 
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn:'1d',                
+                expiresIn: '1d',
             })
             console.log(token)
-            res.send({result, token})
+            res.send({ result, token })
         })
 
         // get a single user by email
-        app.get('/user/:email', async (req, res)=>{
-            const email  = req.params.email
-            const query = {email}
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email }
             const user = await usersCollection.findOne(query)
             console.log(user)
             res.send(user)
         })
 
+        // Get all users
+        app.get('/users', async (req, res) => {
+            let query = {}
+            const users = await usersCollection.find(query).toArray()
+            console.log(users)
+            res.send(users)
+        })
         // Save a booking
-        app.post('/bookings', async(req, res)=>{
+        app.post('/bookings', async (req, res) => {
             const bookingData = req.body
             const result = await bookingsCollection.insertOne(bookingData)
             console.log(result)
@@ -63,31 +70,40 @@ async function run(){
         })
 
         // Get all bookings
-        app.get('/bookings', async(req,res)=>{
+        app.get('/bookings', async (req, res) => {
             let query = {}
             const email = req.query.email
-            if(email){
+            if (email) {
                 query = {
-                    guestEmail:email
-                }                
+                    guestEmail: email
+                }
             }
             const bookings = await bookingsCollection.find(query).toArray()
             console.log(bookings)
             res.send(bookings)
         })
 
+        
+        // Add a home
+        app.post('/homes', async (req, res) => {
+            const homeData = req.body
+            const result = await homesCollection.insertOne(homeData)
+            console.log(result)
+            res.send(result)
+        })
+
         console.log('Database Connected...')
-    } finally{
+    } finally {
 
     }
 }
 
-run().catch(err=>console.error(err))
+run().catch(err => console.error(err))
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send(`Server is running...`)
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server is running...on ${port}`)
 })
